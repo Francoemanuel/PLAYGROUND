ea02b247d7513e528f4c2e0571ce8abf1cdd6ccc
Franco <francoemanuel039@gmail.com>
2026-06-06 18:22:52 -0300

    proyecto de practica finalizado

--- minilibrary/models.py
+++ minilibrary/models.py
@@ -14,6 +14,9 @@ class Author(models.Model):
 class Genre(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
@@ -25,6 +28,10 @@ class Book(models.Model):
     genres = models.ManyToManyField(Genre, related_name='books')
     recommended_by = models.ManyToManyField(
         get_user_model(), through="Recommendation", related_name="recommendations")
+
+    class Meta:
+        verbose_name = "Libro"
+        verbose_name_plural = "Libros"
 
     def __str__(self):
         return self.title
